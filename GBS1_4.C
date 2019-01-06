c97197464e294093e4a3d89637094cef32e51d3c
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-06 22:01:00 +0530

    Add files via upload

---
+++ GBS1_4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ char t;
+ int z;
+ //scanf("%d",&z);
+// while(z>0)
+ //{
+ scanf("%c",&t);
+ if((t>=65)&&(t<=97))
+ printf("alphabet");
+ else
+  printf("invalid");
+  // z--;
+    //  }
+getch();
+}
