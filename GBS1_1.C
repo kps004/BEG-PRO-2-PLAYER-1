66aadcafa9849de3fd592f8ffae552dd1ab999d8
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-06 21:30:48 +0530

    Add files via upload

---
+++ GBS1_1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+int z,a;
+scanf("%d",&z);
+while(z>0)
+{
+scanf("%d",&a);
+if(a%2==0)
+printf("even number");
+
+else
+     printf("odd number");z--;
+}
+ getch();
+}
