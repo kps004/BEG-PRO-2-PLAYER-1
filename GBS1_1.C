506a218b1a24d01bd8c17122a2037dc0a5394ebe
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-06 21:36:21 +0530

    Update GBS1_1.C

--- GBS1_1.C
+++ GBS1_1.C
@@ -6,12 +6,20 @@ int z,a;
 scanf("%d",&z);
 while(z>0)
 {
+     
 scanf("%d",&a);
+     if(a>=0){
 if(a%2==0)
 printf("even number");
 
 else
-     printf("odd number");z--;
+     printf("odd number");
+     }
+     else
+          printf("invalid");
+}
+     z--;
+          
 }
  getch();
 }
