b279386ff0f5b356ec558b1dec69860384d93665
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-06 22:08:53 +0530

    Add files via upload

---
+++ GBS1_7.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+   int t,z;
+   clrscr();
+   scanf("%d",&z);
+   while(z>0)
+   {  scanf("%d",&t);
+       while(t>0)
+       {
+	     printf("Hello\n");
+	     t--;
+
+       }
+
+   z--;
+   }
+
+
+getch();
+}
