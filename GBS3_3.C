6de9b0f0020a54ec80314a96500201561056a6f9
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-20 19:59:18 +0530

    Create GBS3_3.C

---
+++ GBS3_3.C
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  unsigned long int a[10000],max=0,n,i;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+      if(max<a[i])
+      {
+          max=a[i];
+      }
+  }
+  printf("%d",max);
+    return 0;
+}
