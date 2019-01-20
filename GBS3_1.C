c7b61a20f34b4d525a08101570941bdd0cc098d1
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-20 19:50:45 +0530

    Create GBS3_1.C

---
+++ GBS3_1.C
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int t=0,n,a,d,i;
+    scanf("%d%d%d",&n,&a,&d);
+    for(i=1;i<=n;i++)
+    {
+    t=t+a+(i-1)*d;
+    }
+    printf("%d",t);
+    return 0;
+}
