726b48eaff7e53da084e6a8c023ff46890b0e781
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-20 11:27:54 +0530

    Create gbs2_4.c

---
+++ gbs2_4.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int i,a,z;
+  scanf("%d%d",&a,&z);
+  for(i=a;i<=z;i++)
+  {
+     if(i%2!=0)
+     {
+         printf("%d ",i);
+     }
+  }
+
+    return 0;
+}
