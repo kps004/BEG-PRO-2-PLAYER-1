02b5f38d94a39b99f5d6a57fca21346120424516
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-20 11:53:23 +0530

    Create GBS2_6.C

---
+++ GBS2_6.C
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int i,j,n,a,z,l=0;
+  scanf("%d%d",&a,&z);
+  for(i=a;i<z;i++)
+  {
+     for(j=2;j<i;j++)
+     {
+         if(i%j==0)
+         {
+             l++;
+         }
+     }
+     if(l==0)
+     {
+         printf("%d ",i);
+     }
+     l=0;
+  }
+
+    return 0;
+}
