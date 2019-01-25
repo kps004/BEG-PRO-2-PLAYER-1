f4121c077c2bdb0a259d6143dded481f6b158a44
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-25 20:16:40 +0530

    Create GPS1_9.C

---
+++ GPS1_9.C
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,z,i,j,t,l;
+    scanf("%d%d",&a,&z);
+    for(i=a;i<=z;i++)
+    {   
+        l=0;
+        t=i;
+        for(j=3;j<t;j++)
+        {
+           if(t%j==0)
+           {
+               l++;
+           }
+        
+        }
+        if(l==0)
+        {
+             printf("%d ",t);
+        }
+    }
+    
+    return 0;
+}
