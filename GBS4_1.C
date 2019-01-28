3e8f58fc2a3fa910c9b480b8fba7951d0985bf7a
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-28 22:41:28 +0530

    Update GBS4_1.C

--- GBS4_1.C
+++ GBS4_1.C
@@ -1,13 +1,22 @@
-
-
 #include <stdio.h>
 
 int main()
 {
  char n[10000];
- int i,k;
+ int i,l=0,k;
  gets(n);
  for(i=0;n[i]!='\0';i++);
-printf("%d",i);
+ for(k=0;k<i;k++)
+ {
+     if(n[i]==' ')
+     {
+         
+     }
+     else
+     {
+         l++;
+     }
+ }
+printf("%d",l);
     return 0;
 }
