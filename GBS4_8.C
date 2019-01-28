cde93263692e83498a6a7e64423dd0ebc0de4a0b
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-28 22:05:03 +0530

    Create GBS4_8.C

---
+++ GBS4_8.C
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,t;
+    scanf("%d %d",&a,&b);
+     a=a^b;
+     b=a^b;
+     a=a^b;
+     printf("%d %d",a,b);
+    return 0;
+}
