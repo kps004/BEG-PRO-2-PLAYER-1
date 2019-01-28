479e5c36805c0048f72d9f49815c9ee127e81310
kps004 <36299014+kps004@users.noreply.github.com>
2019-01-28 20:52:46 +0530

    Create GBS4_3.C

---
+++ GBS4_3.C
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char c[10000];
+    int i,l=0,k=0;
+    gets(c);
+    for(l=0;c[l]!='\0';l++);
+    for(i=0;i<l;i++)
+    {
+        if(c[i]==' ')
+        {
+            k++;
+        }
+    }
+printf("%d",k);
+    return 0;
+}
